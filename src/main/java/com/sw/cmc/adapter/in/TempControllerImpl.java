9a97a111b8370be85acebfeb556efbea1b4a3454
Munwoo <fddsgt123@gmail.com>
2025-01-27 23:25:43 +0900

    ✨: swagger, springdoc 설정
    swagger-ui.html 정상 노출 확인
    
    Resolves: #6

--- src/main/java/com/sw/cmc/adapter/in/TempControllerImpl.java
+++ src/main/java/com/sw/cmc/adapter/in/TempControllerImpl.java
@@ -1,5 +1,10 @@
 package com.sw.cmc.adapter.in;
 
+import com.sw.cmc.adapter.in.cmc.dto.CreateCmcInDTO;
+import com.sw.cmc.adapter.in.cmc.web.CmcControllerApi;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * packageName    : com.sw.cmc.adapter.in
  * fileName       : TempControllerImpl
@@ -7,5 +12,10 @@ package com.sw.cmc.adapter.in;
  * date           : 2025-01-24
  * description    :
  */
-public class TempControllerImpl {
+@RestController
+public class TempControllerImpl implements CmcControllerApi {
+    @Override
+    public ResponseEntity<CreateCmcInDTO> getCmcDetail(Long cmcId) throws Exception {
+        return CmcControllerApi.super.getCmcDetail(cmcId);
+    }
 }
