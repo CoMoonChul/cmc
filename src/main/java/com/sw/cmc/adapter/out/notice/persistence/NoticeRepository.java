5190ea37fc029c47fb0bb3839fc2d71d6e37e14f
SeugnGi An <dkstmdrl2003@gmail.com>
2025-02-15 17:22:47 +0900

    ✨:알림함 조회 API 개발중

---
+++ src/main/java/com/sw/cmc/adapter/out/notice/persistence/NoticeRepository.java
@@ -0,0 +1,17 @@
+package com.sw.cmc.adapter.out.notice.persistence;
+
+import com.sw.cmc.adapter.in.notice.dto.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * packageName    : com.sw.cmc.adapter.out.notice.persistence
+ * fileName       : NoticeRepository
+ * author         : An Seung Gi
+ * date           : 2025-02-15
+ * description    :
+ */
+public interface NoticeRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findAllByUserNum(String userNum);
+}
---
+++ src/main/java/com/sw/cmc/application/port/in/notice/NoticeUseCase.java
@@ -0,0 +1,26 @@
+package com.sw.cmc.application.port.in.notice;
+
+import com.sw.cmc.adapter.in.notice.dto.Notification;
+import com.sw.cmc.domain.notice.NoticeDomain;
+
+import java.util.List;
+
+/**
+ * packageName    : com.sw.cmc.application.port.in.notice
+ * fileName       : NoticeUseCase
+ * author         : An Seung Gi
+ * date           : 2025-02-15
+ * description    :
+ */
+public interface NoticeUseCase {
+
+    /**
+     * methodName : selectNotice
+     * author : AN SEUNG GI
+     * description :
+     *
+     * @return notice domain
+     * @throws Exception the exception
+     */
+    List<Notification> selectNotice(String userNum) throws Exception;
+}
