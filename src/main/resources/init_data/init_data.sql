37e72e3e1615bfead453cf44e530dc9e35574355
Munwoo <fddsgt123@gmail.com>
2025-04-26 16:14:44 +0900

    ✨: 알림 템플릿 추가
    #316

--- src/main/resources/init_data/init_data.sql
+++ src/main/resources/init_data/init_data.sql
@@ -5,3 +5,4 @@ insert into notification_template (noti_template_nm, noti_title, noti_content, n
 insert into notification_template (noti_template_nm, noti_title, noti_content, noti_type, create_user) values('그룹나가기','그룹에서 나갔습니다.','{groupNm} 그룹에서 나갔습니다.','NOTI',null);
 insert into notification_template (noti_template_nm, noti_title, noti_content, noti_type, create_user) values('그룹내보내기','그룹에서 퇴출 되었습니다.','{userNum} 님께서 {groupNM} 그룹에서 퇴출하였습니다.','NOTI',null);
 insert into notification_template (noti_template_nm, noti_title, noti_content, noti_type, create_user) values('댓글등록','댓글이 등록되었습니다.','{review-Title} 게시글에 {userNum} 님 께서 댓글을 등록 했습니다.','LINK',null);
+insert into notification_template (noti_template_nm, noti_title, noti_content, noti_type, create_user) values('리뷰요청','리뷰요청이 도착했어요.','{userNm} 님이 리뷰를 요청했어요.<br/>리뷰 제목: {title}','LINK',null);
