dc699a04144b3cb147e87a2dbb728b4a268df983
Munwoo <fddsgt123@gmail.com>
2025-05-30 12:30:57 +0900

    Reafactor. Review condition

---
+++ src/main/java/com/sw/cmc/domain/review/ReviewListCondition.java
@@ -0,0 +1,8 @@
+package com.sw.cmc.domain.review;/**
+ *packageName    : com.sw.cmc.domain.review
+ * fileName       : ReviewListCondition
+ * author         : ihw
+ * date           : 2025. 6. 1.
+ * description    :
+ */public enum ReviewListCondition {
+}
